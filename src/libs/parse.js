ee0d7ddda9a5a235da12e7ebfb3005f18c59a771
Luis Bazan <luisquid.dev@gmail.com>
2026-01-10 19:52:03 +0000

    Pls work

--- src/libs/parse.js
+++ src/libs/parse.js
@@ -47,7 +47,7 @@ export default {
 					if (['category', 'syndicate-to', 'photo'].includes(propKey)) {
 						if(['photo'].includes(propKey))
 						{
-							let original = propValue
+							let original = propValue.ToString()
 							let split = original.replace('static/', '')
 							parsed[propKey] = split
 						}
